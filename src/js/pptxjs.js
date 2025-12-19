712201b735abb57b24d109d45482cb0e872bf724
rwv <i@rwv.dev>
2025-12-19 20:58:19 +0800

    fix                 a = 01; to a= 1

--- src/js/pptxjs.js
+++ src/js/pptxjs.js
@@ -13473,7 +13473,7 @@
             if (alpha !== "") {
                 a = alpha;
             } else {
-                a = 01;
+                a = 1;
             }
             // multiply before convert to HEX
             a = ((a * 255) | 1 << 8).toString(16).slice(1)
