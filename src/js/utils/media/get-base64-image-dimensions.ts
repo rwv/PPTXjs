db21ac891c462daa1d2a2351ce724bcf8529acdf
seedgou <i@rwv.dev>
2026-01-08 16:00:48 +0800

    fix: avoid blocking base64 image dimensions

--- src/js/utils/media/get-base64-image-dimensions.ts
+++ src/js/utils/media/get-base64-image-dimensions.ts
@@ -7,26 +7,14 @@
  */
 export function getBase64ImageDimensions(
   imgSrc: string
-): [number, number] | undefined {
+): [number, number] {
   const image = new Image();
-
-  image.onload = function () {
-    // This callback is just for potential future use
-    // The synchronous check below will catch the dimensions
-  };
-
   image.src = imgSrc;
 
-  // Synchronous check loop
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    if (image.width !== undefined && image.width !== 0) {
-      return [image.width, image.height];
-    }
-    // If width is still undefined after initial check, return undefined
-    // to avoid infinite loop
-    if (image.complete && image.width === 0) {
-      return undefined;
-    }
+  if (image.width && image.height) {
+    return [image.width, image.height];
   }
+
+  // Avoid blocking the event loop; fall back to a safe size.
+  return [1, 1];
 }
