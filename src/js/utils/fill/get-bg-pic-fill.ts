5cb341b5fa6e0aa2cb634e9bd99dad8daffbd2d6
seedgou <i@rwv.dev>
2026-01-10 00:02:18 +0800

    Type background pic fill primitives

--- src/js/utils/fill/get-bg-pic-fill.ts
+++ src/js/utils/fill/get-bg-pic-fill.ts
@@ -22,7 +22,13 @@ import { getPicFill } from "./get-pic-fill";
  * @param index - Slide index for CSS class naming
  * @returns CSS background style string with z-index
  */
-export function getBgPicFill(bgPr: any, sorce: any, warpObj: any, phClr: any, _index: any): string {
+export function getBgPicFill(
+  bgPr: any,
+  sorce: string,
+  warpObj: any,
+  phClr: string | undefined,
+  _index: number | string
+): string {
   const picFillBase64 = getPicFill(sorce, bgPr["a:blipFill"], warpObj);
   const ordr = bgPr["attrs"]["order"];
   const aBlipNode = bgPr["a:blipFill"]["a:blip"];
