fd2edadb6577472a57ea538cb7529af884945eb4
seedgou <i@rwv.dev>
2026-01-09 16:52:57 +0800

    Clean up bg pic fill vars

--- src/js/utils/fill/get-bg-pic-fill.ts
+++ src/js/utils/fill/get-bg-pic-fill.ts
@@ -22,8 +22,7 @@ import { getPicFill } from "./get-pic-fill";
  * @param index - Slide index for CSS class naming
  * @returns CSS background style string with z-index
  */
-export function getBgPicFill(bgPr: any, sorce: any, warpObj: any, phClr: any, index: any): string {
-  let bgcolor;
+export function getBgPicFill(bgPr: any, sorce: any, warpObj: any, phClr: any, _index: any): string {
   const picFillBase64 = getPicFill(sorce, bgPr["a:blipFill"], warpObj);
   const ordr = bgPr["attrs"]["order"];
   const aBlipNode = bgPr["a:blipFill"]["a:blip"];
@@ -32,7 +31,7 @@ export function getBgPicFill(bgPr: any, sorce: any, warpObj: any, phClr: any, in
   if (duotone !== undefined) {
     const clr_ary = [];
     Object.keys(duotone).forEach(function (clr_type) {
-      if (clr_type != "attrs") {
+      if (clr_type !== "attrs") {
         const obj = {};
         obj[clr_type] = duotone[clr_type];
         clr_ary.push(getSolidFill(obj, undefined, phClr, warpObj));
@@ -45,7 +44,7 @@ export function getBgPicFill(bgPr: any, sorce: any, warpObj: any, phClr: any, in
   if (
     aphaModFixNode !== undefined &&
     aphaModFixNode["amt"] !== undefined &&
-    aphaModFixNode["amt"] != ""
+    aphaModFixNode["amt"] !== ""
   ) {
     const amt = parseInt(aphaModFixNode["amt"]) / 100000;
     imgOpacity = "opacity:" + amt + ";";
@@ -54,13 +53,6 @@ export function getBgPicFill(bgPr: any, sorce: any, warpObj: any, phClr: any, in
   const tileNode = getTextByPathList(bgPr, ["a:blipFill", "a:tile", "attrs"]);
   let prop_style = "";
   if (tileNode !== undefined && tileNode["sx"] !== undefined) {
-    const sx = parseInt(tileNode["sx"]) / 100000;
-    const sy = parseInt(tileNode["sy"]) / 100000;
-    const tx = parseInt(tileNode["tx"]) / 100000;
-    const ty = parseInt(tileNode["ty"]) / 100000;
-    const algn = tileNode["algn"];
-    const flip = tileNode["flip"];
-
     prop_style += "background-repeat: round;";
   }
 
@@ -73,7 +65,7 @@ export function getBgPicFill(bgPr: any, sorce: any, warpObj: any, phClr: any, in
       prop_style += "background-size:  100% 100%;;";
     }
   }
-  bgcolor =
+  const bgcolor =
     "background: url(" + picFillBase64 + ");  z-index: " + ordr + ";" + prop_style + imgOpacity;
 
   return bgcolor;
