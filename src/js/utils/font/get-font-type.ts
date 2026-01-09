e9c0407f0a7eb7ff9af8ed1a6d22ad1b8a20d54a
seedgou <i@rwv.dev>
2026-01-10 00:40:10 +0800

    Type font type selector

--- src/js/utils/font/get-font-type.ts
+++ src/js/utils/font/get-font-type.ts
@@ -13,7 +13,12 @@ import { getTextByPathList } from "../object/get-text-by-path-list";
  * @param pFontStyle - Paragraph font style (may specify font index)
  * @returns Font family name or "inherit"
  */
-export function getFontType(node: any, type: any, warpObj: any, pFontStyle: any): string {
+export function getFontType(
+  node: any,
+  type: string | undefined,
+  warpObj: any,
+  pFontStyle: any
+): string {
   let typeface = getTextByPathList(node, ["a:rPr", "a:latin", "attrs", "typeface"]);
 
   if (typeface === undefined) {
