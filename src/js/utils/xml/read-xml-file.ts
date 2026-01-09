5833c6318a594ecf5de9f5777b2f4bdc9d77ea59
seedgou <i@rwv.dev>
2026-01-09 16:35:06 +0800

    Remove unused catch binding

--- src/js/utils/xml/read-xml-file.ts
+++ src/js/utils/xml/read-xml-file.ts
@@ -36,7 +36,7 @@ export function readXmlFile(
     } else {
       return xmlData;
     }
-  } catch (e) {
+  } catch {
     // console.log("error readXmlFile: the file '", filename, "' not exit")
     return null;
   }
