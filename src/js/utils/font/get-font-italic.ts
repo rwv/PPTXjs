9b00c39da19dd36d1bcb68dcea036491d2739d03
seedgou <i@rwv.dev>
2026-01-09 21:00:09 +0800

    Fix font italic lint warnings

--- src/js/utils/font/get-font-italic.ts
+++ src/js/utils/font/get-font-italic.ts
@@ -6,6 +6,6 @@
  * @param slideMasterTextStyles - Master text styles (unused but kept for consistency)
  * @returns "italic" or "inherit"
  */
-export function getFontItalic(node: any, type: any, slideMasterTextStyles: any): string {
+export function getFontItalic(node: any, _type: any, _slideMasterTextStyles: any): string {
   return node["a:rPr"] !== undefined && node["a:rPr"]["attrs"]["i"] === "1" ? "italic" : "inherit";
 }
