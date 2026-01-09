d0c3872369bea0d93fa96b2bcdc2e0d164daa5c8
seedgou <i@rwv.dev>
2026-01-09 16:59:37 +0800

    Use strict checks for bullet colors

--- src/js/utils/bullet/handlers/render-bullet-char.ts
+++ src/js/utils/bullet/handlers/render-bullet-char.ts
@@ -39,31 +39,31 @@ export function renderBulletChar(
     ";";
 
   // Handle different color types
-  if (color_tye == "solid") {
-    if (bultColor[0] !== undefined && bultColor[0] != "") {
+  if (color_tye === "solid") {
+    if (bultColor[0] !== undefined && bultColor[0] !== "") {
       bullet += "color:#" + bultColor[0] + "; ";
     }
-    if (bultColor[1] !== undefined && bultColor[1] != "" && bultColor[1] != ";") {
+    if (bultColor[1] !== undefined && bultColor[1] !== "" && bultColor[1] !== ";") {
       bullet += "text-shadow:" + bultColor[1] + ";";
     }
-  } else if (color_tye == "pattern" || color_tye == "pic" || color_tye == "gradient") {
-    if (color_tye == "pattern") {
+  } else if (color_tye === "pattern" || color_tye === "pic" || color_tye === "gradient") {
+    if (color_tye === "pattern") {
       bullet += "background:" + bultColor[0][0] + ";";
-      if (bultColor[0][1] !== null && bultColor[0][1] !== undefined && bultColor[0][1] != "") {
+      if (bultColor[0][1] !== null && bultColor[0][1] !== undefined && bultColor[0][1] !== "") {
         bullet += "background-size:" + bultColor[0][1] + ";";
       }
-      if (bultColor[0][2] !== null && bultColor[0][2] !== undefined && bultColor[0][2] != "") {
+      if (bultColor[0][2] !== null && bultColor[0][2] !== undefined && bultColor[0][2] !== "") {
         bullet += "background-position:" + bultColor[0][2] + ";";
       }
-    } else if (color_tye == "pic") {
+    } else if (color_tye === "pic") {
       bullet += bultColor[0] + ";";
-    } else if (color_tye == "gradient") {
+    } else if (color_tye === "gradient") {
       const colorAry = bultColor[0].color;
       const rot = bultColor[0].rot;
 
       bullet += "background: linear-gradient(" + rot + "deg,";
       for (let i = 0; i < colorAry.length; i++) {
-        if (i == colorAry.length - 1) {
+        if (i === colorAry.length - 1) {
           bullet += "#" + colorAry[i] + ");";
         } else {
           bullet += "#" + colorAry[i] + ", ";
