bfa4cc3a78f164bec7fc507995163af9e6e76bf8
seedgou <i@rwv.dev>
2026-01-09 13:34:37 +0800

    Revert "refactor: upgrade to official txml v5.2.1 with closure-based order tracking"
    This reverts commit 5ddebac7bf26ba61b6effa311175aeb5db006129.

--- src/js/utils/xml/read-xml-file.ts
+++ src/js/utils/xml/read-xml-file.ts
@@ -1,5 +1,5 @@
 import type { PptxArchive } from "../../archive/pptx-archive";
-import { tXml } from "../vendors/txml-enhanced";
+import { tXml } from "../vendors/txml";
 
 /**
  * Reads and parses an XML file from a PPTX archive
@@ -36,7 +36,7 @@ export function readXmlFile(
     } else {
       return xmlData;
     }
-  } catch {
+  } catch (e) {
     // console.log("error readXmlFile: the file '", filename, "' not exit")
     return null;
   }
