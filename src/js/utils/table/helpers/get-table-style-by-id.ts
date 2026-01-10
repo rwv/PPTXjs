3d02c06934ab880af58d95905f4646d8e6035583
seedgou <i@rwv.dev>
2026-01-10 09:50:42 +0800

    Type table style lookup

--- src/js/utils/table/helpers/get-table-style-by-id.ts
+++ src/js/utils/table/helpers/get-table-style-by-id.ts
@@ -10,26 +10,39 @@
  * @returns Table style definition with attached attribute flags, or undefined
  */
 
+type TableStyleAttrs = {
+  styleId?: string;
+  [key: string]: unknown;
+};
+
+type TableStyleNode = {
+  attrs?: TableStyleAttrs;
+  [key: string]: unknown;
+};
+
 export function getTableStyleById(
   styleId: string | undefined,
-  tableStyles: any,
-  tblStylAttrObj: any
-): any {
+  tableStyles: Record<string, unknown> | undefined,
+  tblStylAttrObj: Record<string, unknown>
+): TableStyleNode | undefined {
   if (styleId === undefined) {
     return undefined;
   }
 
-  const tbleStylList = tableStyles?.["a:tblStyleLst"]?.["a:tblStyle"];
+  const tbleStylList = tableStyles?.["a:tblStyleLst"]?.["a:tblStyle"] as
+    | TableStyleNode
+    | Array<TableStyleNode>
+    | undefined;
   if (tbleStylList === undefined) {
     return undefined;
   }
 
-  let foundStyle: any = undefined;
+  let foundStyle: TableStyleNode | undefined = undefined;
 
   // Handle array of styles
   if (Array.isArray(tbleStylList)) {
     for (let k = 0; k < tbleStylList.length; k++) {
-      if (tbleStylList[k]["attrs"]?.["styleId"] === styleId) {
+      if (tbleStylList[k].attrs?.styleId === styleId) {
         foundStyle = tbleStylList[k];
         break;
       }
@@ -37,7 +50,7 @@ export function getTableStyleById(
   }
   // Handle single style
   else {
-    if (tbleStylList["attrs"]?.["styleId"] === styleId) {
+    if (tbleStylList.attrs?.styleId === styleId) {
       foundStyle = tbleStylList;
     }
   }
