8ad7f2f3e61e778f1f39b1efb1de2cc930106e62
seedgou <i@rwv.dev>
2026-01-10 20:52:35 +0800

    Clarify numbering variables

--- src/js/utils/text/get-num-type-num.ts
+++ src/js/utils/text/get-num-type-num.ts
@@ -6,44 +6,44 @@ import { hebrew2Minus } from "./archaic-numbers";
  * Formats a number according to the specified numbering type
  * Used for bullet lists and numbered items in presentations
  *
- * @param numTyp - The numbering type/format to use
+ * @param numberingType - The numbering type/format to use
  * @param num - The number to format
  * @returns Formatted string with the number and appropriate punctuation
  */
-export function getNumTypeNum(numTyp: string, num: number | string): string {
-  let rtrnNum: string;
+export function getNumTypeNum(numberingType: string, num: number | string): string {
+  let formattedNumber: string;
 
-  switch (numTyp) {
+  switch (numberingType) {
     case "arabicPeriod":
-      rtrnNum = num + ". ";
+      formattedNumber = num + ". ";
       break;
     case "arabicParenR":
-      rtrnNum = num + ") ";
+      formattedNumber = num + ") ";
       break;
     case "alphaLcParenR":
-      rtrnNum = alphaNumeric(num, "lowerCase") + ") ";
+      formattedNumber = alphaNumeric(num, "lowerCase") + ") ";
       break;
     case "alphaLcPeriod":
-      rtrnNum = alphaNumeric(num, "lowerCase") + ". ";
+      formattedNumber = alphaNumeric(num, "lowerCase") + ". ";
       break;
     case "alphaUcParenR":
-      rtrnNum = alphaNumeric(num, "upperCase") + ") ";
+      formattedNumber = alphaNumeric(num, "upperCase") + ") ";
       break;
     case "alphaUcPeriod":
-      rtrnNum = alphaNumeric(num, "upperCase") + ". ";
+      formattedNumber = alphaNumeric(num, "upperCase") + ". ";
       break;
     case "romanUcPeriod":
-      rtrnNum = romanize(num) + ". ";
+      formattedNumber = romanize(num) + ". ";
       break;
     case "romanLcParenR":
-      rtrnNum = romanize(num) + ") ";
+      formattedNumber = romanize(num) + ") ";
       break;
     case "hebrew2Minus":
-      rtrnNum = hebrew2Minus.format(Number(num)) + "-";
+      formattedNumber = hebrew2Minus.format(Number(num)) + "-";
       break;
     default:
-      rtrnNum = String(num);
+      formattedNumber = String(num);
   }
 
-  return rtrnNum;
+  return formattedNumber;
 }
