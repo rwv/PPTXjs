fd4e2955cf245d6f3fa216bb76d9b0fb907aeda6
seedgou <i@rwv.dev>
2026-01-10 21:06:47 +0800

    Clarify table border variables

--- src/js/utils/border/get-table-borders.ts
+++ src/js/utils/border/get-table-borders.ts
@@ -1,8 +1,8 @@
 /**
  * Get table border styles for all sides
  *
- * @param node - The XML node containing border properties
- * @param warpObj - The warp object containing theme and other resources
+ * @param tableBorderNode - The XML node containing border properties
+ * @param warpContext - The warp object containing theme and other resources
  * @returns CSS border style string
  */
 import { getBorder } from "./get-border";
@@ -13,58 +13,58 @@ type BorderSideNode = {
 
 type BorderTableNode = Record<string, BorderSideNode | undefined>;
 
-export function getTableBorders(node: BorderTableNode, warpObj: unknown): string {
+export function getTableBorders(tableBorderNode: BorderTableNode, warpContext: unknown): string {
   let borderStyle = "";
 
-  const bottom = node["a:bottom"];
+  const bottom = tableBorderNode["a:bottom"];
   if (bottom?.["a:ln"] !== undefined) {
-    const obj = {
+    const borderNode = {
       "p:spPr": {
         "a:ln": bottom["a:ln"],
       },
     };
-    const borders = getBorder(obj, undefined, false, "shape", warpObj);
-    if (typeof borders === "string") {
-      borderStyle += borders.replace("border", "border-bottom");
+    const borderCss = getBorder(borderNode, undefined, false, "shape", warpContext);
+    if (typeof borderCss === "string") {
+      borderStyle += borderCss.replace("border", "border-bottom");
     }
   }
 
-  const top = node["a:top"];
+  const top = tableBorderNode["a:top"];
   if (top?.["a:ln"] !== undefined) {
-    const obj = {
+    const borderNode = {
       "p:spPr": {
         "a:ln": top["a:ln"],
       },
     };
-    const borders = getBorder(obj, undefined, false, "shape", warpObj);
-    if (typeof borders === "string") {
-      borderStyle += borders.replace("border", "border-top");
+    const borderCss = getBorder(borderNode, undefined, false, "shape", warpContext);
+    if (typeof borderCss === "string") {
+      borderStyle += borderCss.replace("border", "border-top");
     }
   }
 
-  const right = node["a:right"];
+  const right = tableBorderNode["a:right"];
   if (right?.["a:ln"] !== undefined) {
-    const obj = {
+    const borderNode = {
       "p:spPr": {
         "a:ln": right["a:ln"],
       },
     };
-    const borders = getBorder(obj, undefined, false, "shape", warpObj);
-    if (typeof borders === "string") {
-      borderStyle += borders.replace("border", "border-right");
+    const borderCss = getBorder(borderNode, undefined, false, "shape", warpContext);
+    if (typeof borderCss === "string") {
+      borderStyle += borderCss.replace("border", "border-right");
     }
   }
 
-  const left = node["a:left"];
+  const left = tableBorderNode["a:left"];
   if (left?.["a:ln"] !== undefined) {
-    const obj = {
+    const borderNode = {
       "p:spPr": {
         "a:ln": left["a:ln"],
       },
     };
-    const borders = getBorder(obj, undefined, false, "shape", warpObj);
-    if (typeof borders === "string") {
-      borderStyle += borders.replace("border", "border-left");
+    const borderCss = getBorder(borderNode, undefined, false, "shape", warpContext);
+    if (typeof borderCss === "string") {
+      borderStyle += borderCss.replace("border", "border-left");
     }
   }
 
