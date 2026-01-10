7822be504abe795500c3d728795d84558a52dd4d
seedgou <i@rwv.dev>
2026-01-10 20:44:10 +0800

    Clarify SVG middle stop variables

--- src/js/utils/svg/get-middle-stops.ts
+++ src/js/utils/svg/get-middle-stops.ts
@@ -2,25 +2,25 @@
  * Calculates middle stops for gradient color distribution
  * For example, with 3 middle stops: 0%, 25%, 50%, 75%, 100%
  *
- * @param s - Number of middle stops (not including 0% and 100%)
+ * @param middleStopCount - Number of middle stops (not including 0% and 100%)
  * @returns Array of stop percentage strings
  */
-export function getMiddleStops(s: number): string[] {
-  const sArry: string[] = ["0%", "100%"];
+export function getMiddleStops(middleStopCount: number): string[] {
+  const stopPercentages: string[] = ["0%", "100%"];
 
-  if (s === 0) {
-    return sArry;
+  if (middleStopCount === 0) {
+    return stopPercentages;
   }
 
-  let i = s;
-  while (i--) {
+  let index = middleStopCount;
+  while (index--) {
     // Calculate evenly distributed middle stops
     // Ex: For 3 middle stops, progression will be 25%, 50%, and 75%
-    const middleStop = 100 - (100 / (s + 1)) * (i + 1);
+    const middleStop = 100 - (100 / (middleStopCount + 1)) * (index + 1);
     const middleStopString = middleStop + "%";
-    // Add into sArry before 100%
-    sArry.splice(-1, 0, middleStopString);
+    // Add into stopPercentages before 100%
+    stopPercentages.splice(-1, 0, middleStopString);
   }
 
-  return sArry;
+  return stopPercentages;
 }
