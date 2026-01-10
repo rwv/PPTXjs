4e74ac322b44d00fc188b540fde2270ee50c64fa
seedgou <i@rwv.dev>
2026-01-10 20:47:16 +0800

    Clarify video link parameter

--- src/js/utils/media/is-video-link.ts
+++ src/js/utils/media/is-video-link.ts
@@ -1,11 +1,11 @@
 /**
  * Checks if a string is a valid video URL
  *
- * @param vdoFile - URL string to check
+ * @param videoUrl - URL string to check
  * @returns True if the string is a valid HTTP/HTTPS/FTP URL
  */
-export function isVideoLink(vdoFile: string): boolean {
+export function isVideoLink(videoUrl: string): boolean {
   const urlRegex =
     /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
-  return urlRegex.test(vdoFile);
+  return urlRegex.test(videoUrl);
 }
