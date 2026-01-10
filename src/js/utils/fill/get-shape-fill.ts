49ab4ab950bae21ab63bc7bb9c71669bafd04f8a
seedgou <i@rwv.dev>
2026-01-10 20:38:59 +0800

    Clarify shape fill variables

--- src/js/utils/fill/get-shape-fill.ts
+++ src/js/utils/fill/get-shape-fill.ts
@@ -1,11 +1,11 @@
 /**
  * Get shape fill style
  *
- * @param node - The XML node containing shape properties
- * @param pNode - Parent node (for group fills)
+ * @param shapeNode - The XML node containing shape properties
+ * @param parentNode - Parent node (for group fills)
  * @param isSvgMode - Whether to return SVG format or CSS format
- * @param warpObj - The warp object containing theme and other resources
- * @param source - Source type for image lookup
+ * @param warpContext - The warp object containing theme and other resources
+ * @param sourceType - Source type for image lookup
  * @returns Fill color/style as CSS string or SVG-compatible format
  */
 import { getTextByPathList } from "../object";
@@ -17,106 +17,110 @@ import { getPicFill } from "./get-pic-fill";
 import tinycolor from "tinycolor2";
 
 export async function getShapeFill(
-  node: any,
-  pNode: any,
+  shapeNode: any,
+  parentNode: any,
   isSvgMode: boolean,
-  warpObj: any,
-  source: string
+  warpContext: any,
+  sourceType: string
 ): Promise<any> {
   // 1. presentationML
   // p:spPr/ [a:noFill, solidFill, gradFill, blipFill, pattFill, grpFill]
   // From slide
   // Fill Type:
-  const fillType = getFillType(getTextByPathList(node, ["p:spPr"]));
-  let fillColor;
+  const fillType = getFillType(getTextByPathList(shapeNode, ["p:spPr"]));
+  let fillValue;
 
   if (fillType === "NO_FILL") {
     return isSvgMode ? "none" : "";
   } else if (fillType === "SOLID_FILL") {
-    const shpFill = node["p:spPr"]["a:solidFill"];
-    fillColor = getSolidFill(shpFill, undefined, undefined, warpObj);
+    const shapeFillNode = shapeNode["p:spPr"]["a:solidFill"];
+    fillValue = getSolidFill(shapeFillNode, undefined, undefined, warpContext);
   } else if (fillType === "GRADIENT_FILL") {
-    const shpFill = node["p:spPr"]["a:gradFill"];
-    fillColor = getGradientFill(shpFill, warpObj);
+    const shapeFillNode = shapeNode["p:spPr"]["a:gradFill"];
+    fillValue = getGradientFill(shapeFillNode, warpContext);
   } else if (fillType === "PATTERN_FILL") {
-    const shpFill = node["p:spPr"]["a:pattFill"];
-    fillColor = getPatternFill(shpFill, warpObj);
+    const shapeFillNode = shapeNode["p:spPr"]["a:pattFill"];
+    fillValue = getPatternFill(shapeFillNode, warpContext);
   } else if (fillType === "PIC_FILL") {
-    const shpFill = node["p:spPr"]["a:blipFill"];
-    fillColor = await getPicFill(source, shpFill, warpObj);
+    const shapeFillNode = shapeNode["p:spPr"]["a:blipFill"];
+    fillValue = await getPicFill(sourceType, shapeFillNode, warpContext);
   }
 
   // 2. drawingML namespace
-  if (fillColor === undefined) {
-    const clrName = getTextByPathList(node, ["p:style", "a:fillRef"]);
-    const idx = parseInt(getTextByPathList(node, ["p:style", "a:fillRef", "attrs", "idx"]));
-    if (idx === 0 || idx === 1000) {
+  if (fillValue === undefined) {
+    const fillRefNode = getTextByPathList(shapeNode, ["p:style", "a:fillRef"]);
+    const fillRefIndex = parseInt(
+      getTextByPathList(shapeNode, ["p:style", "a:fillRef", "attrs", "idx"])
+    );
+    if (fillRefIndex === 0 || fillRefIndex === 1000) {
       // no fill
       return isSvgMode ? "none" : "";
-    } else if (idx > 0 && idx < 1000) {
+    } else if (fillRefIndex > 0 && fillRefIndex < 1000) {
       // <a:fillStyleLst> fill
-    } else if (idx > 1000) {
+    } else if (fillRefIndex > 1000) {
       // <a:bgFillStyleLst>
     }
-    fillColor = getSolidFill(clrName, undefined, undefined, warpObj);
+    fillValue = getSolidFill(fillRefNode, undefined, undefined, warpContext);
   }
 
   // 3. is group fill
-  if (fillColor === undefined) {
-    const grpFill = getTextByPathList(node, ["p:spPr", "a:grpFill"]);
-    if (grpFill !== undefined) {
+  if (fillValue === undefined) {
+    const groupFillNode = getTextByPathList(shapeNode, ["p:spPr", "a:grpFill"]);
+    if (groupFillNode !== undefined) {
       // get parent fill style
-      const grpShpFill = pNode["p:grpSpPr"];
-      const spShpNode = { "p:spPr": grpShpFill };
-      return await getShapeFill(spShpNode, node, isSvgMode, warpObj, source);
+      const groupShapePropsNode = parentNode["p:grpSpPr"];
+      const groupShapeNode = { "p:spPr": groupShapePropsNode };
+      return await getShapeFill(groupShapeNode, shapeNode, isSvgMode, warpContext, sourceType);
     } else if (fillType === "NO_FILL") {
       return isSvgMode ? "none" : "";
     }
   }
 
-  if (fillColor !== undefined) {
+  if (fillValue !== undefined) {
     if (fillType === "GRADIENT_FILL") {
       if (isSvgMode) {
-        return fillColor;
+        return fillValue;
       } else {
-        const colorAry = fillColor.color;
-        const rot = fillColor.rot;
+        const gradientColors = fillValue.color;
+        const gradientRotation = fillValue.rot;
 
-        let bgcolor = "background: linear-gradient(" + rot + "deg,";
-        for (let i = 0; i < colorAry.length; i++) {
-          if (i === colorAry.length - 1) {
-            bgcolor += "#" + colorAry[i] + ");";
+        let gradientStyle = "background: linear-gradient(" + gradientRotation + "deg,";
+        for (let i = 0; i < gradientColors.length; i++) {
+          if (i === gradientColors.length - 1) {
+            gradientStyle += "#" + gradientColors[i] + ");";
           } else {
-            bgcolor += "#" + colorAry[i] + ", ";
+            gradientStyle += "#" + gradientColors[i] + ", ";
           }
         }
-        return bgcolor;
+        return gradientStyle;
       }
     } else if (fillType === "PIC_FILL") {
       if (isSvgMode) {
-        return fillColor;
+        return fillValue;
       } else {
-        return "background-image:url(" + fillColor + ");";
+        return "background-image:url(" + fillValue + ");";
       }
     } else if (fillType === "PATTERN_FILL") {
-      let bgPtrn = "",
-        bgSize = "",
-        bgPos = "";
-      bgPtrn = fillColor[0];
-      if (fillColor[1] !== null && fillColor[1] !== undefined && fillColor[1] !== "") {
-        bgSize = " background-size:" + fillColor[1] + ";";
+      let backgroundPattern = "",
+        backgroundSizeStyle = "",
+        backgroundPositionStyle = "";
+      backgroundPattern = fillValue[0];
+      if (fillValue[1] !== null && fillValue[1] !== undefined && fillValue[1] !== "") {
+        backgroundSizeStyle = " background-size:" + fillValue[1] + ";";
       }
-      if (fillColor[2] !== null && fillColor[2] !== undefined && fillColor[2] !== "") {
-        bgPos = " background-position:" + fillColor[2] + ";";
+      if (fillValue[2] !== null && fillValue[2] !== undefined && fillValue[2] !== "") {
+        backgroundPositionStyle = " background-position:" + fillValue[2] + ";";
       }
-      return "background: " + bgPtrn + ";" + bgSize + bgPos;
+      return (
+        "background: " + backgroundPattern + ";" + backgroundSizeStyle + backgroundPositionStyle
+      );
     } else {
       if (isSvgMode) {
-        const color = tinycolor(fillColor);
-        fillColor = color.toRgbString();
-        return fillColor;
+        const color = tinycolor(fillValue);
+        fillValue = color.toRgbString();
+        return fillValue;
       } else {
-        return "background-color: #" + fillColor + ";";
+        return "background-color: #" + fillValue + ";";
       }
     }
   } else {
