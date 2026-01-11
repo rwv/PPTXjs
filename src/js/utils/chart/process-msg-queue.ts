cc73f6df44a68052bf19c0541767b09fe336f0d2
seedgou <i@rwv.dev>
2026-01-11 12:44:51 +0800

    Clarify chart queue variables

--- src/js/utils/chart/process-msg-queue.ts
+++ src/js/utils/chart/process-msg-queue.ts
@@ -3,15 +3,15 @@ import { processSingleMsg } from "./process-single-msg";
 /**
  * Process a queue of chart messages
  *
- * @param queue - Array of message objects with data property containing chart info
+ * @param messageQueue - Array of message objects with data property containing chart info
  * @returns True if any message was successfully processed, false otherwise
  */
-export function processMsgQueue(queue: Array<{ data: unknown }>): boolean {
-  let anySucceeded = false;
-  for (let i = 0; i < queue.length; i++) {
-    if (processSingleMsg(queue[i].data)) {
-      anySucceeded = true;
+export function processMsgQueue(messageQueue: Array<{ data: unknown }>): boolean {
+  let processedAny = false;
+  for (const message of messageQueue) {
+    if (processSingleMsg(message.data)) {
+      processedAny = true;
     }
   }
-  return anySucceeded;
+  return processedAny;
 }
