1fad883dc65d9e939d237c7aa2d50c078f22090f
rwv <i@rwv.dev>
2026-01-13 17:10:48 +0800

    refactor: simplify video link checks

--- src/js/utils/media/is-video-link.ts
+++ src/js/utils/media/is-video-link.ts
@@ -9,7 +9,16 @@ type IsVideoLinkOptions = {
 };
 
 export function isVideoLink({ videoUrl }: IsVideoLinkOptions): boolean {
-  const urlRegex =
-    /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
-  return urlRegex.test(videoUrl);
+  const trimmedUrl = videoUrl.trim();
+  if (trimmedUrl.length === 0) {
+    return false;
+  }
+
+  try {
+    const parsed = new URL(trimmedUrl);
+    const protocol = parsed.protocol.toLowerCase();
+    return protocol === "http:" || protocol === "https:" || protocol === "ftp:";
+  } catch {
+    return false;
+  }
 }
