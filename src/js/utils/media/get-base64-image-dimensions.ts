7ad922e3ccd4a9c94828d8d167f41209d400ffae
rwv <i@rwv.dev>
2026-01-14 09:36:38 +0800

    refactor: guard empty base64 dimensions

--- src/js/utils/media/get-base64-image-dimensions.ts
+++ src/js/utils/media/get-base64-image-dimensions.ts
@@ -11,6 +11,9 @@ type GetBase64ImageDimensionsOptions = {
 export function getBase64ImageDimensions({
   imgSrc,
 }: GetBase64ImageDimensionsOptions): [number, number] {
+  if (!imgSrc.trim()) {
+    return [1, 1];
+  }
   const base64Match = imgSrc.match(/base64,(.*)$/);
   const base64Data = (base64Match ? base64Match[1] : imgSrc).replace(/\s/g, "");
 
